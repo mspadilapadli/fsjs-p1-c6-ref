462ad9fe848f74666128957a60affc3adec91380
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-08 23:23:02 +0700

    R1-part1: routers

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,28 @@
 const router = require("express").Router();
-const { Employee, Store } = require("../models");
+const Controller = require("../controllers/controller");
 
-router.get("/", (req, res) => {
-    res.send(test);
-});
+router.get("/", Controller.readStores);
+router.get("/stores", Controller.readStores);
+router.get("/employees", Controller.readEmployees);
+router.get("/stores/add", Controller.getFormStore);
+router.post("/stores/add", Controller.postAddStore);
+
+router.get("/stores/:storedId/employees/add", Controller.getFormEmployee);
+router.post("/stores/:storedId/employees/add", Controller.postAddEmployee);
+
+router.get(
+    "/stores/:id/employees/:employeeId/edit",
+    Controller.getFormEmployee
+);
+router.post(
+    "/stores/:id/employees/:employeeId/edit",
+    Controller.postEditEmployee
+);
+router.get(
+    "/stores/:id/employees/:employeeId/delete",
+    Controller.employeeDelete
+);
+
+router.get("/stores/:storedId", Controller.storeDetail);
 
 module.exports = router;
