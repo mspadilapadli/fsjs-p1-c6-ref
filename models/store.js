c9ee70b34bd5eb5bb5683911f3c9a78c865cad45
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-10 21:38:55 +0700

    model store: hooks beforeCreate

--- models/store.js
+++ models/store.js
@@ -11,6 +11,18 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
             Store.hasMany(models.Employee, { foreignKey: "StoreId" });
         }
+        static async employeeFee(data) {
+            try {
+                // console.log(data.Employees.salary, "model");
+                let totalFee = 0;
+                data.Employees.map((item) => (totalFee += item.salary));
+
+                // console.log(totalFee);
+                return helper.formatCurrency(totalFee);
+            } catch (error) {
+                throw error;
+            }
+        }
     }
     Store.init(
         {
@@ -22,6 +34,29 @@ module.exports = (sequelize, DataTypes) => {
         {
             sequelize,
             modelName: "Store",
+            hooks: {
+                beforeCreate(instance, option) {
+                    // let code1 = "";
+                    // if (instance.category === "Mart") {
+                    //     code1 = "001";
+                    // } else if (instance.category === "Midi") {
+                    //     code1 = "002";
+                    // } else if (instance.category === "Express") {
+                    //     code1 = "003";
+                    // }
+                    let time = new Date(
+                        instance.dataValues.createdAt
+                    ).getTime();
+                    let code =
+                        instance.category === "Mart"
+                            ? "001"
+                            : instance.category === "Midi"
+                            ? "002"
+                            : "003";
+
+                    instance.code = `${code} - ${time}`;
+                },
+            },
         }
     );
     return Store;
